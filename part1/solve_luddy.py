b8b21d14f93d2ec1e9d110a9737fd9b9088e92db
Milan Chheta <michheta@iu.edu>
2019-10-01 18:37:24 -0400

    initial commit

--- part1/solve_luddy.py
+++ part1/solve_luddy.py
@@ -31,22 +31,58 @@ def printable_board(row):
 # return a list of possible successor states
 def successors(state):
     (empty_row, empty_col) = ind2rowcol(state.index(0))
-    return [ (swap_tiles(state, empty_row, empty_col, empty_row+i, empty_col+j), c) \
+    r=[ (swap_tiles(state, empty_row, empty_col, empty_row+i, empty_col+j), c) \
              for (c, (i, j)) in MOVES.items() if valid_index(empty_row+i, empty_col+j) ]
+    return r
 
 # check if we've reached the goal
 def is_goal(state):
     return sorted(state[:-1]) == list(state[:-1]) and state[-1]==0
-    
-# The solver! - using BFS right now
+
+"""def heuristic_a(state):
+    c=0
+    for i in range(0,len(state)):
+        if state[i]!=i+1 and state[i]!=0 :
+            c=c+1
+    return c
+"""
+def heuristic_a(state):
+    c=0
+    for i in range(0,16):
+       if state[i]!=0:
+            (x1,y1)=ind2rowcol(state[i]-1)
+            (x2,y2)=ind2rowcol(i)
+            c=c+(abs(x2-x1)+abs(y2-y1))
+    return c
+
+
+
+# The solver! - using BFS right nowol,m
 def solve(initial_board):
-    fringe = [ (initial_board, "") ]
+    count=0
+    h_a=heuristic_a(initial_board)
+    fringe = [ (initial_board, "" ) ]
+    visited=[]
+    hlist=[h_a]
+
     while len(fringe) > 0:
-        (state, route_so_far) = fringe.pop()
+        (state, route_so_far)= fringe.pop()
+        hlist.pop(hlist.index(min(hlist)))
+        if state in visited:
+            continue
+        visited.append(state)
         for (succ, move) in successors( state ):
-            if is_goal(succ):
-                return( route_so_far + move )
-            fringe.insert(0, (succ, route_so_far + move ) )
+            if is_goal(state):
+                count = count + 1
+                print(count)
+                return (route_so_far + move)
+            count =count+1
+            h_a = heuristic_a(succ)
+            fringe.insert(0, (succ, route_so_far + move ))
+            hlist.insert(0,h_a+len(route_so_far)+1)
+
+
+
     return False
 
 # test cases
@@ -68,6 +104,7 @@ if __name__ == "__main__":
     print("Start state: \n" +"\n".join(printable_board(tuple(start_state))))
 
     print("Solving...")
+
     route = solve(tuple(start_state))
     
     print("Solution found in " + str(len(route)) + " moves:" + "\n" + route)
--- part2/route.py
+++ part2/route.py
@@ -1,3 +1,13 @@
 #!/usr/local/bin/python3
 
 # put your routing program here!
+import sys
+# test cases
+if __name__ == "__main__":
+
+  start_city=sys.argv[1]
+  end_city=sys.argv[2]
+  cost_function=sys.argv[3]
+  print(start_city,end_city,cost_function)
+
+
